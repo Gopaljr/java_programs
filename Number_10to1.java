6bc2be38dd6ea8a3314cd800adb25332daf08b67
Gopalakrishnan <123711954+Gopaljr@users.noreply.github.com>
2025-08-21 20:47:09 +0530

    Add files via upload

---
+++ Number_10to1.java
@@ -0,0 +1,47 @@
+
+package comm;
+public class Number_10to1 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num == 10) {
+            System.out.println(10);
+            num = num - 1;
+        }
+        if (num == 9) {
+            System.out.println(9);
+            num = num - 1;
+        }
+        if (num == 8) {
+            System.out.println(8);
+            num = num - 1;
+        }
+        if (num == 7) {
+            System.out.println(7);
+            num = num - 1;
+        }
+        if (num == 6) {
+            System.out.println(6);
+            num = num - 1;
+        }
+        if (num == 5) {
+            System.out.println(5);
+            num = num - 1;
+        }
+        if (num == 4) {
+            System.out.println(4);
+            num = num - 1;
+        }
+        if (num == 3) {
+            System.out.println(3);
+            num = num - 1;
+        }
+        if (num == 2) {
+            System.out.println(2);
+            num = num - 1;
+        }
+        if (num == 1) {
+            System.out.println(1);
+        }
+    }
+}
